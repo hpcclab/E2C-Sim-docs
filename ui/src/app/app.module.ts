346c5250b00f0a8cb044230a7447193a50d843e9
theokoles <gabrieltrahan777@hotmail.com>
2022-09-17 14:57:25 -0500

    Stabilizes initial layout structure

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/components/header/header.component';
+import { NavMenuComponent } from './modules/components/nav-menu/nav-menu.component';
+import { NotFoundComponent } from './modules/components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavMenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
